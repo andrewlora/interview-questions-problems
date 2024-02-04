74de639543eb2c42e2f52020d7123c3e9e74f0bb
Andres Lora <jalc.andres.lora@gmail.com>
2024-02-04 14:17:50 -0700

    Added Stack implementation with hash table and arrays

---
+++ 04_stacks/stackImplementation.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const temp = this.top;
+      this.top = newNode;
+      this.top.prev = temp;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    this.top = this.top.prev;
+    this.length--;
+    return this;
+  }
+  //isEmpty
+}
+
+const myStack = new Stack();
+console.log(myStack.push('google'));
+console.log(myStack.push('google2'));
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.pop());
+//Discord
+//ZTM
+//google
