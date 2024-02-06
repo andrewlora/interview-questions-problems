ab8375878df048253d75a685752a3e9f7a761e5a
Andres Lora <jalc.andres.lora@gmail.com>
2024-02-06 14:28:27 -0700

    Added graphs implementation

---
+++ 07_graphs/graphExample.js
@@ -0,0 +1,25 @@
+// Edge List
+const graph = [
+  [0, 2],
+  [2, 3],
+  [2, 1],
+  [1, 3],
+];
+
+// Adjacent List
+const graphAdj = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// Adjacent Matrix
+const graphMatrix = [
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0],
+];
+
+const graphObj = {
+  0: [0, 0, 1, 0],
+  1: [0, 0, 1, 1],
+  2: [1, 1, 0, 1],
+  3: [0, 1, 1, 0],
+};
