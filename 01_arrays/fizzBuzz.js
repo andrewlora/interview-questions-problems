07793ba5975b339a0309696f110777cfacfe4e5a
Andres Lora <andreslora@Andress-MacBook-Pro.local>
2024-07-12 23:11:55 -0700

    Added FizzBuzz algorithm and launch debug whit node

--- 01_arrays/fizzBuzz.js
+++ 01_arrays/fizzBuzz.js
@@ -1,5 +1,7 @@
 // FizzBuzz
-// Question: Write a function that prints numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+// Question: Write a function that prints numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
 
 function fizzBuzz() {
   for (let i = 1; i <= 100; i++) {
@@ -15,4 +17,4 @@ function fizzBuzz() {
   }
 }
 
-fizzBuzz();
+console.log(fizzBuzz());
